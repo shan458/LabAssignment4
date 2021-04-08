69960d3f6c0fbdb5f87354e3f147d04509bccfef
shan458 <77813639+shan458@users.noreply.github.com>
2021-04-07 21:43:42 -0700

    finish pseudocode

--- lab_4.py
+++ lab_4.py
@@ -7,3 +7,24 @@ We first set max_tries = 3. Then we create a loop to iterate three times. For ea
 If the user could not guess the correct answer within the max_tries, then print "You have used up your allotment of guesses.", then print "The correct answer is 'California'".
 """
 
+"""
+main
+    question = "What is the capital of California? "
+    answer = "Sacramento"
+    ask(question, answer)
+
+ask
+    tries = 0
+    loop three times
+        increment tries by 1
+        ask user input()
+        check to see if user input is equal to answer
+            if so, print "Correct" then exit loop
+    if not correct
+        print to the user "You have used up your allotment of guesses."
+        print the correct answer "The correct answer is 'Sacramento'"
+
+main
+"""
+
+
